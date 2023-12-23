7dc03682fea4efcbf9139c2f07a719faafae8696
Daniela Vornic <daniela.vornic@gmail.com>
2023-12-23 22:57:21 +0200

    Update professors and sidebar

--- types/professor.ts
+++ types/professor.ts
@@ -1,6 +1,9 @@
+import { Course } from "./course";
+
 export type Professor = {
-    id: number,
-    name: string,
-    subject: string,
-    days: [number]
-}
+  id: number;
+  name: string;
+  type: "theory" | "seminar" | "lab";
+  courses: Partial<Course>[];
+  days: number[];
+};
